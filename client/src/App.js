75408cca483baf9f5ec2512a28fede2b96147c3e
jojijac0b <jojijacob22@gmail.com>
2018-09-04 10:16:49 -0700

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -14,50 +14,13 @@ class App extends Component {
     // Get the passwords and store them in state
     fetch('/api/hello')
       .then(res => res.json())
-      .then(array => this.setState({ array }));
+      .then(array => this.setState({ array: array }));
   }
 
   render() {
-    const { array } = this.state;
+    const { array } = this.state.array;
 
-    return (
-      <div className="App">
-        {/* Render the passwords if we have them */}
-        {array.length ? (
-          <div>
-            <h1>5 Hellos.</h1>
-            <ul className="passwords">
-              {/*
-                Generally it's bad to use "index" as a key.
-                It's ok for this example because there will always
-                be the same number of passwords, and they never
-                change positions in the array.
-              */}
-              {array.map((hello, index) =>
-                <li key={index}>
-                  {hello}
-                </li>
-              )}
-            </ul>
-            <button
-              className="more"
-              onClick={this.getHellos}>
-              Get More
-            </button>
-          </div>
-        ) : (
-          // Render a helpful message otherwise
-          <div>
-            <h1>No passwords :(</h1>
-            <button
-              className="more"
-              onClick={this.getHellos}>
-              Try Again?
-            </button>
-          </div>
-        )}
-      </div>
-    );
+    return (<h1> REACT SERVER IS UP<h1>);
   }
 }
 
