f2bea58faa50c28b28f61ca280909f9a8ed82d30
Aleksandar Djordjevic <adjordjevic@firstbeatmedia.com>
2018-09-20 00:19:00 +0200

    Initial develop environment for server and client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (<div>Frontend and Backend up and running</div>);
-  }
+    render() {
+        return (<div>Frontend and Backend up and running</div>);
+    }
 }
 
 export default App;
---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/../client/build')));
+
+app.get('/api/apitest', (req, res) => {
+    // Generate some passwords
+    const testString = 'Api call test';
+
+    // Return them as json
+    res.json(testString);
+
+    console.log(`Api call successful`);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Stream41 listening on ${port}`);
